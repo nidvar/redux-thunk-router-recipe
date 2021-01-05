e6773c6ebc01074d8b4d934864ba1b2e67c2a423
Nidvar <djayshine87@hotmail.com>
2021-01-05 18:10:56 +1100

    first commit

---
+++ src/components/Searchbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <form>
+                    <input />
+                    <button>SEARCH</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
